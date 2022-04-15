b72d034dcbaf682b922a80751a034363c891bbe2
K4L1B3 <luizhlimagomes28@gmail.com>
2022-04-14 21:57:26 -0300

    backup

--- src/main/java/com/AprendoSpring/aprendendoSpring/controllers/PedidoController.java
+++ src/main/java/com/AprendoSpring/aprendendoSpring/controllers/PedidoController.java
@@ -1,43 +1,80 @@
-// package com.AprendoSpring.aprendendoSpring.controllers;
+package com.AprendoSpring.aprendendoSpring.controllers;
 
-// import java.util.List;
+import java.util.List;
+import java.util.Optional;
 
-// import com.AprendoSpring.aprendendoSpring.models.Pedido;
-// import com.AprendoSpring.aprendendoSpring.service.PedidoService;
+import com.AprendoSpring.aprendendoSpring.models.Pedido;
+import com.AprendoSpring.aprendendoSpring.service.PedidoService;
 
-// import org.apache.catalina.connector.Response;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("/pedidos")
-// public class PedidoController {
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
     
-//     @Autowired
-//     private PedidoService pedidoService;
+    @Autowired
+    private PedidoService pedidoService;
 
-//     //CREATE AND SAVE
-//     @PostMapping("/abrirPedido")
-//     public ResponseEntity<Object> savePedido(Pedido pedido){
-//         return ResponseEntity.status(HttpStatus.CREATED).body(pedidoService.save(pedido));
-//     }
+    //CREATE AND SAVE
+    @PostMapping("/abrirPedido")
+    public ResponseEntity<Object> savePedido(Pedido pedido){
+        return ResponseEntity.status(HttpStatus.CREATED).body(pedidoService.save(pedido));
+    }
 
-//     //List All
-//     @GetMapping("/verTodosPedidos")
-//     public List<Pedido> findAllPedidos(){
-//         return pedidoService.findAll();
-//     }
+    //List All
+    @GetMapping("/verTodosPedidos")
+    public List<Pedido> findAllPedidos(){
+        return pedidoService.findAll();
+    }
 
-//     //List By Id
-//     @GetMapping("/pedidoId/{id}")
-//     public ResponseEntity<Object> pedidosPorId(@PathVariable value())
+    //List By Id
+    @GetMapping("/pedidoId/{id}")
+    public ResponseEntity<Object> pedidosPorId(@PathVariable (value = "id") Long id){
+        Optional<Pedido> pedidoOptional = pedidoService.findById(id);
 
-// }
+        if(!pedidoOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pedido não existe");
+        } else{
+            return ResponseEntity.status(HttpStatus.OK).body(pedidoOptional.get());
+        }
+    }
+
+    // UPDATE
+    @PutMapping("/pedidoUpdate/{id}")
+    public ResponseEntity<Object> updatePedidos(@PathVariable (value = "id") Long id, @RequestBody Pedido pedido){
+        Optional<Pedido> pedidoOptional = pedidoService.findById(id);
+        if(!pedidoOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pedido não encontrado");
+        }else{
+
+            var atualizarPedido = new Pedido();
+            BeanUtils.copyProperties(pedido, atualizarPedido);
+            pedido.setId(pedidoOptional.get().getId());
+            return ResponseEntity.status(HttpStatus.OK).body(pedidoService.save(pedido)); 
+        }
+    }
+
+    // DELETE
+    @DeleteMapping("pedidoDel/{id}")
+    public ResponseEntity<Object> deletePedido(@PathVariable (value = "id")Long id){
+        Optional<Pedido> pedidoOptional = pedidoService.findById(id);
+        if(!pedidoOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pedido não existe!");
+        }else {
+            return ResponseEntity.status(HttpStatus.OK).body("Pedido deletado com sucesso!");
+        }
+    }
+
+}
--- src/main/java/com/AprendoSpring/aprendendoSpring/service/PedidoService.java
+++ src/main/java/com/AprendoSpring/aprendendoSpring/service/PedidoService.java
@@ -1,25 +1,34 @@
-// package com.AprendoSpring.aprendendoSpring.service;
+package com.AprendoSpring.aprendendoSpring.service;
 
-// import java.util.List;
+import java.util.List;
+import java.util.Optional;
 
-// import com.AprendoSpring.aprendendoSpring.models.Pedido;
-// import com.AprendoSpring.aprendendoSpring.repositories.PedidoRepository;
+import com.AprendoSpring.aprendendoSpring.models.Pedido;
+import com.AprendoSpring.aprendendoSpring.repositories.PedidoRepository;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class PedidoService {
+@Service
+public class PedidoService {
 
-//     @Autowired
-//     private PedidoRepository pedidoRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
-//     public Object save(Pedido pedido) {
-//         return pedidoRepository.save(pedido);
-//     }
+    public Object save(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
 
-//     public List<Pedido> findAll() {
-//         return pedidoRepository.findAll();
-//     }
+    public List<Pedido> findAll() {
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<Pedido> findById(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public void delete(Pedido pedido) {
+         pedidoRepository.delete(pedido);
+    }
     
-// }
+}
