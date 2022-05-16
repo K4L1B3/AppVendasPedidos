b9ae6a0cc5edd42f4cb727cae64e1ead11112671
K4L1B3 <luizhlimagomes28@gmail.com>
2022-05-16 00:11:27 -0300

    Config: configuração de ambiente da prod e dev para build com geração de arquivos JAR ou WAR atráves do terminal

--- src/main/java/com/AprendoSpring/aprendendoSpring/AprendendoSpringApplication.java
+++ src/main/java/com/AprendoSpring/aprendendoSpring/AprendendoSpringApplication.java
@@ -2,12 +2,13 @@ package com.AprendoSpring.aprendendoSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 // Spring Web MVC para rodar o Swagger
 @EnableWebMvc
 @SpringBootApplication
-public class AprendendoSpringApplication {
+public class AprendendoSpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AprendendoSpringApplication.class, args);
